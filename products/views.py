b21ea3c0f57dde632bc5d0fbb727d09b9bc47d1b
usyfi <usyf.ibragimov@yandex.com>
2024-02-03 12:43:31 +0300

    1punkt

--- products/views.py
+++ products/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'products/index.html')
+
+def products (request):
+    return render(request, 'products/products.html')
