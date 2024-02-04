070ef703b42611db83769df9969f60b7df7e11ce
usyfi <usyf.ibragimov@yandex.com>
2024-02-04 12:32:44 +0300

    2punkt

--- store/urls.py
+++ store/urls.py
@@ -20,6 +20,6 @@ from products.views import index, products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index, name='index'),
+    path('', index, name='index'),
     path('products/', products, name='products'),
 ]
